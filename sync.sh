b0f5f3827d2ce4f056cd13eebbeb951222f913c0
Fabien Poussin <fabien.poussin@gmail.com>
2019-06-06 11:33:18 +0200

    Updated script to use local svn mirror.

--- sync.sh
+++ sync.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-LOCAL_REPO=$HOME/chibios-git-svn
-SVN_REPO=https://svn.osdn.net/svnroot/chibios/
-GIT_REPO=git@github.com:ChibiOS/ChibiOS.git
+BASE=$HOME/mirrors
+LOCAL_SVN=$BASE/chibios-svn
+LOCAL_GIT=$BASE/chibios-git
+REMOTE_SVN=https://svn.osdn.net/svnroot/chibios/
+REMOTE_GIT=git@github.com:ChibiOS/ChibiOS.git
+SVN_UUID=27425a3e-05d8-49a3-a47f-9c15f0e5edd8
 
 if [ -z "$1" ]
   then
@@ -13,25 +16,38 @@ fi
 if [[ "$1" == "init" ]]
 then
 
-if [ -d $LOCAL_REPO ]
+if [ -d $BASE ]
   then
-  echo repo already exists at $LOCAL_REPO
+  echo repo already exists at $BASE
   exit 1
 fi
 
+mkdir -p $BASE
+echo Creating SVN Mirror...
+svnadmin create $LOCAL_SVN $REMOTE_SVN
+mkdir $LOCAL_SVN/hooks
+echo '#!/bin/sh' > $LOCAL_SVN/hooks/pre-revprop-change
+chmod 755 $LOCAL_SVN/hooks/pre-revprop-change
+
+echo Syncing SVN mirror for the first time, this will take a while...
+svnsync sync file://$LOCAL_SVN
+
 echo Cloning from git...
-git clone $GIT_REPO $LOCAL_REPO
+git clone $REMOTE_GIT $LOCAL_GIT
 
 echo Adding SVN config
-cp authors.txt $LOCAL_REPO/
-cat << EOF >> $LOCAL_REPO/.git/config
+cp authors.txt $BASE/
+cat << EOF >> $LOCAL_GIT/.git/config
 [svn-remote "svn"]
-        url = $SVN_REPO
+        url = file://$LOCAL_SVN
         fetch = trunk:refs/remotes/origin/master
-        branches = branches/{stable_17.6.x,stable_18.2.x,stable_19.1.x}:refs/remotes/origin/*
+        fetch = branches/stable_18.2.x:refs/remotes/origin/stable_18.2.x
+        fetch = branches/stable_19.1.x:refs/remotes/origin/stable_19.1.x
         tags = tags/ver*:refs/tags/*
+        rewriteRoot = svn://svn.code.sf.net/p/chibios/svn
+        rewriteUUID = $SVN_UUID
 [svn]
-        authorsfile = $LOCAL_REPO/authors.txt
+        authorsfile = $BASE/authors.txt
         useLogAuthor = true
 EOF
 
@@ -40,8 +56,11 @@ fi
 if [[ "$1" == "update" ]]
 then
 
+echo Syncing SVN mirror
+svnsync sync file://$LOCAL_SVN
+
 echo Updating SVN refs
-cd $LOCAL_REPO
+cd $LOCAL_GIT
 git svn fetch
 
 fi
@@ -50,7 +69,7 @@ if [[ "$1" == "push" ]]
 then
 
 echo Pushing to git mirror
-cd $LOCAL_REPO
+cd $LOCAL_GIT
 git push
 
 fi
